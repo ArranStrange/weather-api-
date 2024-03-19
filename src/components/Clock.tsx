fc31ea406590d3b5a5fb08bdeb4cfdcbe7ee157d
Arran <arranstrange@googlemail.com>
2024-03-19 10:35:55 +0000

    notes added

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,26 +1,28 @@
 import SunMoonClock from "../assets/Backgrounds/Time 0.png";
 
+//props from app.tsx extracting local time from API and passing searchInputFocus function
 interface ClockProps {
   localtime: string;
   searchInputFocused: boolean;
 }
 
 export function Clock(props: ClockProps) {
+  //function to
   const imageRotation = () => {
     console.log(props.localtime);
-
     // Check if props.localtime is undefined or not a string
     if (!props.localtime || typeof props.localtime !== "string") {
-      return null;
+      return null; //returns null if local time is invalid
     }
-
+    // Split localtime into date and time
     const timeComponents = props.localtime.split(" ");
 
     // Check if timeComponents array has expected format
     if (timeComponents.length !== 2) {
-      return null;
+      return null; //returns null is local time is invalid
     }
 
+    // Extract hour from time component and convert to number
     const hour = parseInt(timeComponents[1].split(":")[0], 10); // Provide index 0 to get the hours
 
     // Check if hour is a valid number
@@ -42,6 +44,7 @@ export function Clock(props: ClockProps) {
   const rotationStyle = imageRotation();
 
   return (
+    //renders clock face with rotation based on hour if searchInputFocus is false
     !props.searchInputFocused && (
       <img
         className="clock"
